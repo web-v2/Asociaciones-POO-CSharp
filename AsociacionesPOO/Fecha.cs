d5b1a5992bd2e5ea0d51bf988dee4723f886ee31
Samir Vergara V <svergara3@misena.edu.co>
2023-08-21 23:00:54 -0500

    Add project files.

---
+++ AsociacionesPOO/Fecha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsociacionesPOO
+{
+    internal class Fecha
+    {
+        //Atributos
+        private int dia, mes, anio;
+
+        public int dia { get; set; }
+        public int mes { get; set; }
+
+
+    }
+}
---
+++ AsociacionesPOO/Form1.cs
@@ -0,0 +1,10 @@
+namespace AsociacionesPOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
